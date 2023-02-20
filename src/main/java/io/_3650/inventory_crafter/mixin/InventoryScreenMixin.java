b553b741b95bf909db44d3533e5ab938afa63f7f
LegoMaster3650 <37251737+LegoMaster3650@users.noreply.github.com>
2023-02-19 20:35:07 -0600

    1.0.1 - Hopefully fix issue #1

--- src/main/java/io/_3650/inventory_crafter/mixin/InventoryScreenMixin.java
+++ src/main/java/io/_3650/inventory_crafter/mixin/InventoryScreenMixin.java
@@ -6,9 +6,14 @@ import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import io._3650.inventory_crafter.InventoryCrafter;
 import io._3650.inventory_crafter.client.InventoryCrafterClient;
+import io._3650.inventory_crafter.network.InventoryCrafterPacket;
+import io._3650.inventory_crafter.network.NetworkHandler;
 import io._3650.inventory_crafter.registry.config.Config;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.network.chat.Component;
@@ -22,10 +27,21 @@ public abstract class InventoryScreenMixin<T extends AbstractContainerMenu> exte
 	public InventoryScreenMixin(T menu, Inventory inventory, Component title) {
 		super(menu, inventory, title);
 	}
+	
+	@Inject(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/inventory/InventoryScreen;addRenderableWidget(Lnet/minecraft/client/gui/components/events/GuiEventListener;)Lnet/minecraft/client/gui/components/events/GuiEventListener;"))
+	private void inventory_crafter_init(CallbackInfo ci) {
+		InventoryCrafterClient.inventoryButton = new ImageButton(leftPos + Config.CLIENT.buttonLeftX.get(), topPos + Config.CLIENT.buttonTopY.get(), 20, 18, 0, 0, 19, InventoryCrafterClient.BUTTON, clicked -> {
+			Minecraft minceraft = Minecraft.getInstance(); //resource leak warning wont shut if I do it in one line
+			if (InventoryCrafter.hasCraftingTable(minceraft.player)) NetworkHandler.sendToServer(new InventoryCrafterPacket());
+		});
+		
+		InventoryCrafterClient.inventoryButton.visible = InventoryCrafter.hasCraftingTable(minecraft.player);
+		this.addRenderableWidget(InventoryCrafterClient.inventoryButton);
+	}
 
 	@Inject(method = "lambda$init$0", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/ImageButton;setPosition(II)V", shift = Shift.AFTER))
 	private void inventory_crafter_moveButton(Button button, CallbackInfo cb) {
-		if (InventoryCrafterClient.inventoryButton != null) InventoryCrafterClient.inventoryButton.setPosition(leftPos + Config.CLIENT.buttonLeftX.get(), topPos + Config.CLIENT.buttonTopY.get());
+		if (InventoryCrafterClient.inventoryButton != null) InventoryCrafterClient.inventoryButton.setPosition(this.leftPos + Config.CLIENT.buttonLeftX.get(), this.topPos + Config.CLIENT.buttonTopY.get());
 	}
 	
 }
